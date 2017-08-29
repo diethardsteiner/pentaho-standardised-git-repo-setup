f334a4b34a136d828a6d41b6819c53f927826841
Diethard Steiner <diethardsteiner@users.noreply.github.com>
2017-08-29 08:53:36 +0100

    base dir added so that it can run from crontab

--- utilities/shell-scripts/artefacts/project-config/run_jb_name.sh
+++ utilities/shell-scripts/artefacts/project-config/run_jb_name.sh
@@ -2,5 +2,7 @@
 JOB_PATH="your_project_name"
 JOB_NAME="jb_name"
 
+BASE_DIR=$(dirname 0)
+echo "The shell script is running from following directory: ${BASE_DIR}"
 # the repo name has to be the env name
-source ./wrapper.sh $JOB_PATH $JOB_NAME
+source ${BASE_DIR}/wrapper.sh $JOB_PATH $JOB_NAME
