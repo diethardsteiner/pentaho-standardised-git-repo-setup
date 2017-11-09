186709c64665d90ddb0e657c8dfd53a7f5d8af3f
dsteiner <diehtard.steiner@gmail.com>
2017-11-09 22:56:17 +0000

    crontab support added

--- artefacts/project-config/run_jb_name.sh
+++ artefacts/project-config/run_jb_name.sh
@@ -2,7 +2,9 @@
 JOB_PATH="your_project_name"
 JOB_NAME="jb_name"
 
-BASE_DIR=$(dirname 0)
+#BASE_DIR=$(dirname 0)
+# to make it run with crontab as well
+BASE_DIR="$( cd "$( /usr/bin/dirname "$0" )" && pwd )"
 echo "The shell script is running from following directory: ${BASE_DIR}"
 # the repo name has to be the env name
 source ${BASE_DIR}/wrapper.sh $JOB_PATH $JOB_NAME
