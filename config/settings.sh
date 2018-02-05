09f5d9a9848ee99f4b4de1585f3517c3bab7d6b4
Diethard Steiner <diethardsteiner@users.noreply.github.com>
2018-02-05 14:48:01 +0000

    gitignore files added to accepted file extensions

--- config/settings.sh
+++ config/settings.sh
@@ -23,9 +23,9 @@ export PSGRS_GIT_HOOKS_CHECKS_NEW_FILES_ONLY="N"
 # pipe delimited list of accepted database connection names
 export PSGRS_PDI_ACCEPTED_DATABASE_CONNECTION_NAMES_REGEX="^module\_.*|^hive_generic$|^impala_generic$|^mysql_process_control$"
 # regex to identify valid file extensions in a code repo
-export PSGRS_GIT_CODE_REPO_ACCEPTED_FILE_EXTENSIONS_REGEX="cda$|cdfde$|css$|csv$|html$|jpeg$|js$|json$|kdb$|kjb$|ktr$|md$|png$|prpt$|prpti$|sql$|sh$|svg$|txt$|wcdf$|xanalyzer$|xmi$|xml$"
+export PSGRS_GIT_CODE_REPO_ACCEPTED_FILE_EXTENSIONS_REGEX="cda$|cdfde$|css$|csv$|gitignore$|html$|jpeg$|js$|json$|kdb$|kjb$|ktr$|md$|png$|prpt$|prpti$|sql$|sh$|svg$|txt$|wcdf$|xanalyzer$|xmi$|xml$"
 # regex to identify valid file extensions in a config repo
-export PSGRS_GIT_CONFIG_REPO_ACCEPTED_FILE_EXTENSIONS_REGEX="md$|properties$|sh$|json$|xml$|txt$|csv$"
+export PSGRS_GIT_CONFIG_REPO_ACCEPTED_FILE_EXTENSIONS_REGEX="gitignore$|md$|properties$|sh$|json$|xml$|txt$|csv$"
 # regex specifying the accepted git branch names
 export PSGRS_GIT_REPO_ACCEPTED_BRANCH_NAMES_REGEX="^master$|^dev$|^feature\_.+|^release\_.+|^hotfix\_.+"
 # regex specfying any words that should not show up in file and folder names
